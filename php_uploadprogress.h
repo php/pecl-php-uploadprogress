820b4d30b9a5f6f87613f5a2f297b496fe97ee7a
Christian Stocker <chregu@php.net>
2009-03-15 17:49:39 +0000

    prepare 1.0.0 release

--- php_uploadprogress.h
+++ php_uploadprogress.h
@@ -50,7 +50,7 @@ extern "C" {
 extern zend_module_entry uploadprogress_module_entry;
 #define phpext_uploadprogress_ptr &uploadprogress_module_entry
 
-#define PHP_UPLOADPROGRESS_VERSION "0.9.3-dev"
+#define PHP_UPLOADPROGRESS_VERSION "1.0.0"
 
 #ifdef PHP_WIN32
 #define PHP_UPLOADPROGRESS_API __declspec(dllexport)
