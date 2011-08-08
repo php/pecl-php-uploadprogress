b2e80642b84f3638e71ab50eff25135d1c255d6d
Christian Stocker <chregu@php.net>
2011-08-08 09:52:53 +0000

    Fix compile error on 5.4 due to magic-quotes removal

--- uploadprogress.c
+++ uploadprogress.c
@@ -457,11 +457,12 @@ static void uploadprogress_file_php_get_contents(char *id, char *fieldname, long
         /* uses mmap if possible */
         if ((len = php_stream_copy_to_mem(stream, &contents, maxlen, 0)) > 0) {
 
+#if PHP_API_VERSION < 20100412
             if (PG(magic_quotes_runtime)) {
                 contents = php_addslashes(contents, len, &newlen, 1 TSRMLS_CC);
                 len = newlen;
             }
-
+#endif
             RETVAL_STRINGL(contents, len, 0);
         } else if (len == 0) {
             RETVAL_EMPTY_STRING();
