d9ed39c3e5fafb7f6d8d9a7ef56d0e037d683753
Remi Collet <remi@php.net>
2021-09-29 09:24:40 +0200

    use stub to generate arginfo

---
+++ uploadprogress.stub.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @generate-function-entries
+ * @generate-legacy-arginfo
+ */
+
+function uploadprogress_get_info(string $identifier): ?array {};
+
+function uploadprogress_get_contents(string $identifier, string $fieldname, int $maxlen=-1): ?string {};
+
---
+++ uploadprogress_arginfo.h
@@ -0,0 +1,23 @@
+/* This is a generated file, edit the .stub.php file instead.
+ * Stub hash: 2d041ae6d6a3d972168fe0cc71978048f694a39c */
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_uploadprogress_get_info, 0, 1, IS_ARRAY, 1)
+	ZEND_ARG_TYPE_INFO(0, identifier, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_uploadprogress_get_contents, 0, 2, IS_STRING, 1)
+	ZEND_ARG_TYPE_INFO(0, identifier, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, fieldname, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, maxlen, IS_LONG, 0, "-1")
+ZEND_END_ARG_INFO()
+
+
+ZEND_FUNCTION(uploadprogress_get_info);
+ZEND_FUNCTION(uploadprogress_get_contents);
+
+
+static const zend_function_entry ext_functions[] = {
+	ZEND_FE(uploadprogress_get_info, arginfo_uploadprogress_get_info)
+	ZEND_FE(uploadprogress_get_contents, arginfo_uploadprogress_get_contents)
+	ZEND_FE_END
+};
